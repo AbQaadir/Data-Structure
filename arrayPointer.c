64a2595a00f05fe86b128adabf1698f8992846d5
AbQaadir <qaadir.inbox@gmail.com>
2024-02-25 04:26:18 +0530

    arr itself is an array

---
+++ arrayPointer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int *p;
+
+    p = arr;
+
+
+    int length = sizeof(arr)/sizeof(arr[0]);
+    for (int i = 0; i < length; i++) {
+        printf("The value of arr[%d] is %d\n", i, arr[i]);
+        printf("The value of arr[%d] is %d\n", i, *(p+i));
+        printf("The address of arr[%d] is %p\n", i, &arr[i]);
+        printf("The address of arr[%d] is %p\n", i, p+i);
+        printf("\n");
+    }
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+int length = sizeof(arr)/sizeof(arr[0]);
+
+    for (int i = 0; i < length; i++) {
+        printf("The value of arr[%d] is %d\n", i, arr[i]);
+        printf("The value of arr[%d] is %d\n", i, *(p+i));
+        printf("The address of arr[%d] is %p\n", i, &arr[i]);
+        printf("The address of arr[%d] is %p\n", i, p+i);
+        printf("\n");
+    }
