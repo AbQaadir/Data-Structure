a9730fdd615f1af243a149a19ad1a1fbd2ad9dfe
AbQaadir <qaadir.inbox@gmail.com>
2024-03-05 06:13:03 +0530

    File Handling

--- filr.c
+++ filr.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-
-void openFile(char *fileName);
 
 int main() {
-    char fileName[100];
-    printf("Enter the file name: ");
-    scanf("%s", fileName);
-    openFile(fileName);
+    char sentence[100]; // Assuming a maximum length of 100 characters
+
+    printf("Enter a sentence: ");
+    fgets(sentence, sizeof(sentence), stdin); // Read input including spaces
+
+    printf("You entered: %s", sentence);
+
     return 0;
-}
-
-
-// open a file in read mode
-void openFile(char *fileName){
-    FILE *file = fopen(fileName, "r");
-    if(file == NULL){
-        printf("Error: File not found\n");
-        return;
-    }
-    char ch;
-    while((ch = fgetc(file)) != EOF){
-        printf("%c", ch);
-    }
-
-    fclose(file);
 }
---
+++ whileloop.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+void main(){
+    int i = 0;
+    do
+    {
+        printf("%d\n", i);
+        i++;
+        
+    } while (i != 5);
+
+    int j = 0;
+    printf("\n");
+
+    while (j < 5)
+    {
+        printf("%d\n", j);
+        j++;
+    }
+    
+}
